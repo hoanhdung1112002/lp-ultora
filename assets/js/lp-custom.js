5493daba140cdb37ad0dd4e7b962ffd74973dba7
hadung2002 <79574082+hoanhdung1112002@users.noreply.github.com>
2024-09-19 11:38:15 +0700

    update file

--- assets/js/lp-custom.js
+++ assets/js/lp-custom.js
@@ -17,15 +17,37 @@ jQuery(function($){
 
     $('.reason-list').slick({
         infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 4,
-        draggable: true,
-        arrows: false,
-        dots: false,
-        autoplay: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows:false,
+        autoplay: true,
         autoplaySpeed: 2000,
         variableWidth:true,
         centerMode: true,
         cssEase: 'linear',
     });
+
+    var windowHeight = $(window).height();
+  
+        $(window).scroll(function() {
+          var scrollPosition = $(window).scrollTop();
+  
+          $('.animated').each(function() {
+            var elementPosition = $(this).offset().top;
+            
+            if (scrollPosition > elementPosition - windowHeight / 1.6) {
+              $(this).addClass('run-animated');
+            }
+          });
+        });
+        function animatedFlash() {
+          setInterval(function () {
+            $('.animated-flash').addClass('run-animated');
+          }, 2000);
+          setInterval(function () {
+            $('.animated-flash').removeClass('run-animated');
+          }, 3000);
+        }
+  
+        animatedFlash();
 });
